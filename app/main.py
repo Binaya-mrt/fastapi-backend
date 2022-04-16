d79f87cd8801b76071a648aa081f209df8e95800
Binaya-mrt <marahathabinaya@gmail.com>
2022-04-16 22:43:43 +0545

    another commit

---
+++ app/main.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI, Response, status,HTTPException,Depends
+from pydantic import BaseModel
+from fastapi.params import Body
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
+from sqlalchemy.orm import Session
+from .database import engine,get_db
+from . import models,schemas
+from typing import List  
+
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+while True  :
+    try:
+        conn = psycopg2.connect(dbname='fastapi', user='postgres', password='swarga', host='localhost',cursor_factory= RealDictCursor)
+        cursor=conn.cursor()
+        print("Connected to database")
+        break
+    except Exception as error:
+        print ("Error while connecting to PostgreSQL", error)
+        time.sleep(3)   
+@app.get("/")
+def root():
+    return {"message": "Hey folks"}  
+
+@app.get("/posts",response_model=List[schemas.Post])
+def get_posts(db:Session=Depends(get_db)):
+    posts= db.query(models.Post).all()
+    return posts
+
+@app.post("/posts",status_code=status.HTTP_201_CREATED,response_model=schemas.Post)
+def create_posts(post:schemas.PostCreate,db:Session=Depends(get_db)):
+    # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+    #  (post.title, post.content, post.published))
+    # new_post= cursor.fetchone()
+    # conn.commit()
+    # print(new_post)
+    new_post= models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post     
+
+@app.get("/posts/{id}",response_model=schemas.Post)
+def get_post(id:int,db:Session=Depends(get_db)):
+    # cursor.execute("""SELECT * FROM posts WHERE id= %s""",(str(id),))
+    # post=cursor.fetchone()
+    post=db.query(models.Post).filter(models.Post.id==id).first()
+    if not post:
+       raise  HTTPException(status_code=404, detail="Post not found")
+    return post
+
+@app.delete("/posts/{id}",status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id:int,db:Session=Depends(get_db)):
+    # cursor.execute("""DELETE FROM posts WHERE id= %s RETURNING *""",(str(id),))
+    # deleted_post=cursor.fetchone()
+    # conn.commit()
+    post=db.query(models.Post).filter(models.Post.id==id)
+    if post.first== None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    post.delete(synchronize_session=False)
+    db.commit()
+    return {"message": "Post deleted"}   
+    
+
+@app.put("/posts/{id}",response_model=schemas.Post)
+def update_post(id:int, updated_post:schemas.PostCreate,db:Session=Depends(get_db)):
+    # cursor.execute("""UPDATE posts SET title=%s, content=%s, published=%s WHERE id = %s RETURNING *""",
+    # (post.title, post.content, post.published,str(id),))
+    # updated_post=cursor.fetchone()
+    # conn.commit()
+   
+    post_query=db.query(models.Post).filter(models.Post.id==id)
+    post=post_query.first()
+    if  post ==None:
+        raise HTTPException(status_code=404, detail=f"Post with id: {id} not found")
+    post_query.update(updated_post.dict(),synchronize_session=False)
+    db.commit() 
+    return post_query.first()
---
+++ app/models.py
@@ -0,0 +1,16 @@
+from cgitb import text
+from datetime import datetime
+import imp
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql import func
+
+from psycopg2 import Timestamp
+from .database import Base
+from sqlalchemy import  Column,Integer,String,Boolean
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True,nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, server_default='TRUE',nullable=False)
+    created_at= Column(TIMESTAMP(timezone=True),server_default=func.now())
