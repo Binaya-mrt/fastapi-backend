9e08eee72c95e045aeb36c36f896a185281b80e7
Binaya-mrt <marahathabinaya@gmail.com>
2022-04-23 07:17:18 +0545

    token aayo token aayo

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,7 +3,7 @@ from requests import Session
 from sqlalchemy.orm import Session
 
 from app import utils
-from .. import database, models, schemas
+from .. import database, models, schemas, oath2
 router = APIRouter(tags=["authentication"])
 
 
@@ -17,5 +17,5 @@ def login(user_credentails: schemas.UserLogin, db: Session = Depends(database.ge
 
     if not utils.verify(user_credentails.password, user.password):
         raise HTTPException(status_code=404, detail=f"Invalid credentails")
-
-    return {"token": "fake-token"}
+    access_token = oath2.create_access_token(data={"user_id": user.id})
+    return {"token": access_token, "token_type": "bearer"}
