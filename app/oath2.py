9bcb7f5a08a687305b8deeefbe4bf88ae9a1bdf1
Binaya-mrt <marahathabinaya@gmail.com>
2022-04-23 15:31:39 +0545

    making some token validation stuffs

--- app/oath2.py
+++ app/oath2.py
@@ -1,5 +1,10 @@
+from fastapi import Depends, HTTPException, status
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
+from . import schemas
+from fastapi.security import OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 # sceret key
 # algorithm used
 # expiration time
@@ -12,9 +17,34 @@ def create_access_token(data: dict):
     print(data)
     to_encode = data.copy()
     print(f"to_encode: {to_encode}")
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     ecodec_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     print(f"ecoded_jwt: {to_encode}")
 
     return ecodec_jwt
+
+
+def verify_acess_token(token: str, credientials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        id: str = payload.get("user_id")
+
+        if id is None:
+            raise credientials_exception
+
+        token_data = schemas.TokenData(id=id)
+
+    except JWTError:
+        raise credientials_exception
+    return token_data
+
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    return verify_acess_token(token, credentials_exception)
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, FastAPI, Response, status, HTTPException, Depends
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from requests import Session
 from sqlalchemy.orm import Session
 
@@ -8,14 +9,16 @@ router = APIRouter(tags=["authentication"])
 
 
 @router.post('/login')
-def login(user_credentails: schemas.UserLogin, db: Session = Depends(database.get_db)):
+def login(user_credentails: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
 
     user = db.query(models.User).filter(
-        models.User.email == user_credentails.email).first()
+        models.User.email == user_credentails.username).first()
     if not user:
-        raise HTTPException(status_code=404, detail=f"Invalid credentails")
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid credentails")
 
     if not utils.verify(user_credentails.password, user.password):
-        raise HTTPException(status_code=404, detail=f"Invalid credentails")
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid credentails")
     access_token = oath2.create_access_token(data={"user_id": user.id})
     return {"token": access_token, "token_type": "bearer"}
