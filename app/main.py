2b52cf2d426c17eba5237431e4805a65317a7a42
Binaya-mrt <marahathabinaya@gmail.com>
2022-05-02 11:57:32 +0545

    Removed all files that are in the .gitignore

--- app/main.py
+++ app/main.py
@@ -4,9 +4,18 @@ from .database import engine, get_db
 from . import models
 from typing import List
 from .routers import posts, users, auth, votes
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# models.Base.metadata.create_all(bind=engine) // Yesko kam aaba alembic le garxa
 
 app.include_router(posts.router)
 app.include_router(users.router)
